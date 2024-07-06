8a559b6d21a2188ad8a08e48a85c20b021c9faf7
menotabek <baqara163@gmail.com>
2024-07-06 18:11:07 +0500

    CDN added to settings.py

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,19 @@ DATABASES = {
     }
 }
 
+AWS_ACCESS_KEY_ID = 'DO00QFKKECEJ66ABV9GD'
+AWS_SECRET_ACCESS_KEY = 'DynSRfM4nLPmigZxiw07x2tw0nt9omPeSuhtGUUHrL8'
+AWS_STORAGE_BUCKET_NAME = 'otabeksmcdn'
+AWS_S3_ENDPOINT_URL = 'https://portfolioscdn.blr1.digitaloceanspaces.com'
+AWS_S3_CUSTOM_DOMAIN = 'portfolioscdn.blr1.digitaloceanspaces.com/' + AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read'
+}
+# Media files configuration
+DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
